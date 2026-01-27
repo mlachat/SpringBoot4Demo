99340bff958f5fb2e245fea2eaec53435938775c
mlachat <markuslachat@gmx.de>
2026-01-27 15:06:30 +0100

    create submodule for sender

--- src/main/java/com/example/elstar/repository/ElstarDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.elstar.repository;
-
-import com.example.elstar.entity.ElstarData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ElstarDataRepository extends JpaRepository<ElstarData, Long> {
-}
